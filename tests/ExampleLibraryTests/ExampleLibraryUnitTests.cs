36487b5aa471880f3677015ef19685b335dcc890
Kangkan Goswami <geekays@gmail.com>
2020-12-28 20:35:35 +0530

    Feature static (#1)
    * Updated the methods to static methods
    
    * Updated test methods to invoke static methods
    
    * Added additional test data
    
    Co-authored-by: Kangkan Goswami <kgoswami4@dxc.com>

--- tests/ExampleLibraryTests/ExampleLibraryUnitTests.cs
+++ tests/ExampleLibraryTests/ExampleLibraryUnitTests.cs
@@ -11,9 +11,10 @@ namespace ExampleLibraryTests
         [InlineData(1, 2, 3)]
         [InlineData(10, 20, 30)]
         [InlineData(5, 6, 11)]
+        [InlineData(50, 6, 56)]
         public void Add_Returns_Expected_Value(int x, int y, int expectedResult)
         {
-            var actualResult = _calculator.Add(x, y);
+            var actualResult = Calculator.Add(x, y);
             Assert.Equal(actualResult, expectedResult);
         }
 
@@ -23,7 +24,7 @@ namespace ExampleLibraryTests
         [InlineData(7, 3, 4)]
         public void Subtract_Returns_ExpectedValue(int x, int y, int expectedResult)
         {
-            var actualResult = _calculator.Subtract(x, y);
+            var actualResult = Calculator.Subtract(x, y);
             Assert.Equal(actualResult, expectedResult);
         }
     }
